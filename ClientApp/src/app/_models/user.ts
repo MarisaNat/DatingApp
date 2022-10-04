6f52822acf023f52c52606aa1031eed157d6c459
Marisa Kraft <marisa.kraft@geoas.de>
2022-10-04 14:55:02 +0200

    End of section 5

---
+++ ClientApp/src/app/_models/user.ts
@@ -0,0 +1,5 @@
+//INterfaces werden in Typescript anders al in C# genutzt. Z.B: einfach um einen Typ zu definieren
+export interface User {
+  username: string;
+  token: string;
+}
--- ClientApp/src/app/nav/nav.component.ts
+++ ClientApp/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from "../_services/account.service";
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {}
-  constructor() { }
+
+  constructor(public accountService: AccountService) {
+  }
 
   ngOnInit(): void {
   }
+
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  logout(){
+    this.accountService.logout();
   }
 
 }
