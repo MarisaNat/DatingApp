ad26c18ba380856cf86ea0cb744e4e279d307971
Marisa Kraft <marisa.kraft@geoas.de>
2022-10-05 10:52:19 +0200

    End of section 7 API

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         [Required] //nennt sich hier data Annotationm können auch RegEx enthalten, um Bedingungen für Variablen-Werte zu setzen
         public string Username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
