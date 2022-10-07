3eb9759998dadd71592202f78cc1c6f22d1c87d2
Marisa Kraft <marisa.kraft@geoas.de>
2022-10-07 12:54:50 +0200

    End of section 12 (API)

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +16,13 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
             _context=context;
             _tokenService = tokenService;
+            _mapper=mapper;
         }
         [HttpPost("register")]
         //ActionResult kann z.B. ein BadRequest sein
@@ -27,19 +30,21 @@ namespace API.Controllers
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
+            var user = _mapper.Map<AppUser>(registerDto); //Alle Daten vom Client
+
             using var hmac = new HMACSHA512(); // using ruft dispose() function der Klasse auf.
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt= hmac.Key
-            };
+          //ergänzen und anpassen der Properties
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt= hmac.Key;
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -53,12 +58,12 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
-                .Include(p=>p.Photos)
+                .Include(p => p.Photos)
                 .SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
             if (user == null) { return Unauthorized("Invalid username"); }
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-            for(var i=0; i<computedHash.Length; i++)
+            for (var i = 0; i<computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) { return Unauthorized("Invalid password"); }
             }
@@ -66,7 +71,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
         }
     }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,17 @@ namespace API.DTOs
         [Required] //nennt sich hier data Annotationm können auch RegEx enthalten, um Bedingungen für Variablen-Werte zu setzen
         public string Username { get; set; }
         [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public  string Gender { get; set; }
+        [Required]
+        public string DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
